e715f4e1bb838686df6a720f11b66d6b0e1d1420
andreimihaitaTS <90057478+andreimihaitaTS@users.noreply.github.com>
2021-10-08 15:00:06 +0300

    |ADD| passed the push unit test

--- src/LinkedList/pairs/andrei-diana/LinkedList.js
+++ src/LinkedList/pairs/andrei-diana/LinkedList.js
@@ -4,4 +4,21 @@ module.exports = class LinkedList {
         this.head = null
         this.tail = null
     }
+    push(value) {
+        const node = new Node(value)
+        this.length++
+        if (this.length == 0) {
+            this.head = node
+        } else if (this.tail) {
+            this.tail.next = node
+        }
+        this.tail = node
+    }
+}
+
+class Node {
+    constructor(value) {
+        this.value = value
+        this.next = null
+    }
 }
